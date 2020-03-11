06321147c1481fbe4f83e4789ab512bd87afc55d
Gavyn Riebau <gavyn.riebau@readify.net>
2020-03-11 13:38:20 +0800

    Add 'incident type' select to complaints page

--- src/components/complaints-form.js
+++ src/components/complaints-form.js
@@ -1,5 +1,16 @@
 import React from 'react';
-import { Button, Grid, TextField, Typography, Container, Paper } from '@material-ui/core';
+import {
+    Button,
+    Grid,
+    TextField,
+    Typography,
+    Container,
+    Paper,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Header from './header';
 
@@ -7,7 +18,7 @@ import Header from './header';
 const useStyles = makeStyles(theme => ({
     paper: {
         marginTop: theme.spacing(3),
-        padding: theme.spacing(2)
+        padding: theme.spacing(3)
     },
     form: {
         marginTop: theme.spacing(2),
@@ -33,7 +44,17 @@ function ComplaintsForm() {
                 </Typography>
                 <form className={classes.form}>
                     <TextField required fullWidth variant="outlined" label="Name" />
-                    <TextField required fullWidth multiline variant="outlined" label="Description of what happened" rows="5" />
+                    <FormControl required fullWidth variant="outlined" className={classes.formControl}>
+                        <InputLabel id="incident-type-label">Incident Type</InputLabel>
+                        <Select
+                            labelId="incident-type-label"
+                            id="incident-type">
+                            <MenuItem value={1}>Case mishandling</MenuItem>
+                            <MenuItem value={2}>Improper staff conduct</MenuItem>
+                            <MenuItem value={3}>Other</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <TextField required fullWidth multiline variant="outlined" label="Description of what happened" rows="8" />
                     <Grid container justify="flex-end">
                         <Button variant="contained" color="primary" type="submit">Submit</Button>
                     </Grid>
